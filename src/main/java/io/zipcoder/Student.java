9a99d16bc4032697cb7986ad04e76da6394aa291
ConnorC04 <connorjcasey@iCloud.com>
2025-03-13 16:02:11 -0400

    Kris fix it

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,44 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
+
 public class Student {
+
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores = new ArrayList<>();
+
+    public Student(String firstName, String lastName, ArrayList<Double> examScores){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = examScores;
+    }
+
+
+    public void setFirstName(){
+        this.firstName = firstName;
+    }
+
+    public void setLastName(){
+        this.lastName = lastName;
+    }
+
+    public void setExamScores(ArrayList<Double> examScores) {
+        this.examScores = examScores;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public ArrayList<Double> getExamScores() {
+        return examScores;
+    }
+
+
 }
